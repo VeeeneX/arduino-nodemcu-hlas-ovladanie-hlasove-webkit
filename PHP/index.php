a19b5a5f44e2c4e8b2bef02ab4b3b4e23af9e046
Martin <martinius96@gmail.com>
2017-12-05 13:22:32 +0100

    konverzia na strane servera

--- PHP/index.php
+++ PHP/index.php
@@ -49,6 +49,43 @@
   }else{
     $preklad = htmlspecialchars($preklad);
      $preklad = trim( $preklad );
-    file_put_contents("preklady.txt", $preklad); }
+     if ($preklad=="Zapni LED1" || $preklad=="Zapni LED 1" || $preklad=="Zopni LED 1" || $preklad=="Zopni led 1" || $preklad=="Zapni led 1" || $preklad=="Zapni led1" ){
+      $preklad=="Zapni LED 1";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Zapni LED2" || $preklad=="Zapni LED 2" || $preklad=="Zopni LED 2" || $preklad=="Zopni led 2" || $preklad=="Zapni led 2" || $preklad=="Zapni led2"){
+          $preklad=="Zapni LED 2";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Zapni LED3" || $preklad=="Zapni LED 3" || $preklad=="Zopni LED 2" || $preklad=="Zopni led 3" || $preklad=="Zapni led 3" || $preklad=="Zapni led3"){
+            $preklad=="Zapni LED 3";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Zapni LED4" || $preklad=="Zapni LED 4" || $preklad=="Zopni LED 4" || $preklad=="Zopni led 4" || $preklad=="Zapni led 4" || $preklad=="Zapni led4"){
+              $preklad=="Zapni LED 4";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Vypni LED1" || $preklad=="Vypni LED 1" || $preklad=="Vypni led 1" || $preklad=="Vypni led1"){
+      $preklad=="Vypni LED 1";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Vypni LED2" || $preklad=="Vypni LED 2" ||  $preklad=="Vypni led 2" || $preklad=="Vypni led2"){
+          $preklad=="Vypni LED 2";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Vypni LED3" || $preklad=="Vypni LED 3" ||  $preklad=="Vypni led 3" || $preklad=="Vypni led3"){
+            $preklad=="Vypni LED 3";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Vypni LED4" || $preklad=="Vypni LED 4"  || $preklad=="Vypni led 4" || $preklad=="Vypni led4"){
+              $preklad=="Vypni LED 4";
+      file_put_contents("preklady.txt", $preklad);
+     }else if ($preklad=="Zapni" || $preklad=="Zepni" || $preklad=="Zopni" || $preklad=="Zapni všetko" || $preklad=="Zapni diody" || $preklad=="Spusť diody"){
+              $preklad=="Zapni";
+      file_put_contents("preklady.txt", $preklad);
+     }
+     else if ($preklad=="Vypni" || $preklad=="Vypnúť" || $preklad=="Vypni všetko" || $preklad=="Vypni diódy"){
+              $preklad=="Vypni";
+      file_put_contents("preklady.txt", $preklad);
+     }else{
+     $myfile = fopen("neuspesnypreklad.txt", "wr") or die("Nepodarilo sa otvoriť súbor!");
+$txt = $preklad;
+fwrite($myfile, $txt);
+fclose($myfile);
+     }
 
-}?>
+}     }
+?>
