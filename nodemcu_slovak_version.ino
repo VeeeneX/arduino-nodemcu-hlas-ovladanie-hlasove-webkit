f5e5342768898a1b9f183e32a49bf93cd4129040
Martin <martinius96@gmail.com>
2017-12-05 13:09:17 +0100

    slovak version

--- nodemcu_slovak_version.ino
+++ nodemcu_slovak_version.ino
@@ -8,7 +8,7 @@ const char* ssid = "wifimeno";
 const char* password = "heslo";
 const char* host = "vasastranka.sk"; //bez https a www
 const int httpsPort = 443;
-const char* fingerprint = "13 9f 87 1d b1 85 be e6 bd 73 c1 8d 04 63 58 99 f0 32 43 92";
+const char* fingerprint = "13 9f 87 1d b1 85 be e6 bd 73 c1 8d 04 63 58 99 f0 32 43 92"; // odtlacok certifikatu SHA1
 void setup() {
  Serial.begin(115200);
  Serial.println();
@@ -41,7 +41,7 @@ void loop() {
  } else {
  Serial.println("certifikat nezhodny");
  }
-String url = "/ preklady.txt";
+String url = "/preklady.txt";
  Serial.print("poziadavka na adresu: ");
  Serial.println(url);
  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
@@ -53,44 +53,36 @@ String url = "/ preklady.txt";
  String line = client.readStringUntil('\n');
 
  if (line == "\r") {
- Serial.println("hlavicka obdrzana");
+ Serial.println("Response prijata");
  break;
  }
  }
  String line = client.readStringUntil('\n');
- Serial.println("Odpoved bola:");
+ Serial.println("Vratena premenna: ");
  Serial.println("==========");
  Serial.println(line);
- if(line=="Zapni LED 1" || line =="Zapni led 1" || line =="Zapni let 1" || line =="Zapni LED jeden" || line =="Zapni LED1" || line =="Zapni
-led1"){
+ if(line=="Zapni LED 1"){
  digitalWrite(led1, HIGH);
  }
- if(line=="Vypni LED 1" || line =="Vypni led 1" || line =="Vypni let 1" || line =="Vypni LED jeden" || line =="Vypni LED1" || line =="Vypni
-led1"){
+ if(line=="Vypni LED 1"){
  digitalWrite(led1, LOW);
  }
- if(line=="Zapni LED 2" || line =="Zapni led 2" || line =="Zapni let 2" || line =="Zapni LED dva" || line =="Zapni LED2" || line =="Zapni
-led2"){
+ if(line=="Zapni LED 2"){
  digitalWrite(led2, HIGH);
  }
- if(line=="Vypni LED 2" || line =="Vypni led 2" || line =="Vypni let 2" || line =="Vypni LED dva" || line =="Vypni LED2" || line =="Vypni
-led2"){
+ if(line=="Vypni LED 2"){
  digitalWrite(led2, LOW);
  }
- if(line=="Zapni LED 3" || line =="Zapni led 3" || line =="Zapni let 3" || line =="Zapni LED tri" || line =="Zapni LED3" || line =="Zapni
-led3"){
+ if(line=="Zapni LED 3"){
  digitalWrite(led3, HIGH);
  }
- if(line=="Vypni LED 3" || line =="Vypni led 3" || line =="Vypni let 3" || line =="Vypni LED tri" || line =="Vypni LED3" || line =="Vypni
-led3"){
+ if(line=="Vypni LED 3"){
  digitalWrite(led3, LOW);
  }
- if(line=="Zapni LED 4" || line =="Zapni led 4" || line =="Zapni let 4" || line =="Zapni LED styri" || line =="Zapni LED4" || line =="Zapni
-led4"){
+ if(line=="Zapni LED 4"){
  digitalWrite(led4, HIGH);
  }
- if(line=="Vypni LED 4" || line =="Vypni led 4" || line =="Vypni let 4" || line =="Vypni LED styri" || line =="Vypni LED4" || line=="Vypni
-led4"){
+ if(line=="Vypni LED 4"){
  digitalWrite(led4, LOW);
  }
  if(line=="Vypni"){
@@ -107,7 +99,3 @@ led4"){
  digitalWrite(led3, HIGH);
  digitalWrite(led4, HIGH);
  }
-
-
- Serial.println("==========");
- Seria
