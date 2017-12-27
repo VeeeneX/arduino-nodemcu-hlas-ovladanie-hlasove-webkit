07150383e10944a7f595a0254ee20bb7bbbc36d5
Martin <martinius96@gmail.com>
2017-12-27 15:46:51 +0100

    Doplnok

--- PHP_cz/ajax.php
+++ PHP_cz/ajax.php
@@ -1,4 +1,10 @@
 <?php 
+// Autor: Martin Chlebovec alias: martinius96
+// Podpora: https://www.paypal.me/Chlebovec
+// Osobný web: https://arduino.php5.sk
+// Email kontakt: martinius96@gmail.com
+// Facebook kontakt: 100001242570317
+// Používajte v súhade s licenciou!
    $preklad = $_POST['preklad'];
    $preklad = htmlspecialchars($preklad);
      $preklad = trim( $preklad );
