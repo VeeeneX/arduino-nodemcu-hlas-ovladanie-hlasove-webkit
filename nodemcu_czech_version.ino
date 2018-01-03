114deaf22e28d606b114fab398c43eb2310ca224
Martin <martinius96@gmail.com>
2018-01-03 20:57:31 +0100

    W5100 czech language

--- nodemcu_czech_version.ino
+++ nodemcu_czech_version.ino
@@ -1,107 +1,80 @@
-// Autor: Martin Chlebovec alias: martinius96
-// Podpora: https://www.paypal.me/Chlebovec
-// Osobný web: https://arduino.php5.sk
-// Email kontakt: martinius96@gmail.com
-// Facebook kontakt: 100001242570317
-// Používajte v súhade s licenciou!
-#include <ESP8266WiFi.h> //kniznica importovana v Arduine core, testovana verzia 2.3.0
-#include <WiFiClientSecure.h> //kniznica importovana v Arduine core, testovana verzia 2.3.0
-const int led1 = 16;
-const int led2 = 5;
-const int led3 = 4;
-const int led4 = 0;
-const char* ssid = "wifimeno";
-const char* password = "heslo";
-const char* host = "vasastranka.sk"; //bez https a www
-const int httpsPort = 443;
-const char* fingerprint = "13 9f 87 1d b1 85 be e6 bd 73 c1 8d 04 63 58 99 f0 32 43 92"; // odtlacok certifikatu SHA1
-void setup() {
- Serial.begin(115200);
- Serial.println();
- pinMode(led1, OUTPUT);
- pinMode(led2, OUTPUT);
- pinMode(led3, OUTPUT);
- pinMode(led4, OUTPUT);
- Serial.print("pripajam na ");
- Serial.println(ssid);
- WiFi.begin(ssid, password);
- while (WiFi.status() != WL_CONNECTED) {
- delay(500);
- Serial.print(".");
- }
- Serial.println("");
- Serial.println("WiFi pripojene");
- Serial.println("IP adresa: ");
- Serial.println(WiFi.localIP());
+#include <SPI.h>
+#include <Ethernet.h>
+int led = 6;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //physical mac address
+char serverName[] = "www.mywebsite.com"; // zoomkat's test web page server
+IPAddress ip(192, 168, 2, 40);
+EthernetClient client;
+String readString;
+int x=0; //for counting line feeds
+char lf=10; //line feed character
+void setup(){
+pinMode(led, OUTPUT);
+ if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    Ethernet.begin(mac, ip);
+  }
+  Serial.begin(9600); 
 }
-void loop() {
- WiFiClientSecure client;
- Serial.print("pripajam sa na ");
- Serial.println(host);
- if (!client.connect(host, httpsPort)) {
- Serial.println("pripojenie neuspesne");
- return;
- }
- if (client.verify(fingerprint, host)) {
- Serial.println("certifikat zhodny");
- } else {
- Serial.println("certifikat nezhodny");
- }
-String url = "/PHP_cz/preklady.txt";
- Serial.print("poziadavka na adresu: ");
- Serial.println(url);
- client.print(String("GET ") + url + " HTTP/1.1\r\n" +
- "Host: " + host + "\r\n" +
- "User-Agent: NodeMCU\r\n" +
- "Connection: close\r\n\r\n");
- Serial.println("poziadavka vykonana");
- while (client.connected()) {
- String line = client.readStringUntil('\n');
-
- if (line == "\r") {
- Serial.println("Response prijata");
- break;
- }
- }
- String line = client.readStringUntil('\n');
- Serial.println("Vratena premenna: ");
- Serial.println("==========");
- Serial.println(line);
- if(line=="Zapni LED 1"){
+void loop(){
+if (client.connect(serverName, 80)) {  //starts client connection, checks for connection
+    Serial.println("connected");
+    client.println("GET /PHP_cz/preklady.txt HTTP/1.1"); //download text
+    client.println("Host: www.mywebsite.com");
+    client.println("Connection: close");  //close 1.1 persistent connection  
+    client.println(); //end of get request
+  } 
+  else {
+    Serial.println("connection failed"); //error message if no client connect
+    Serial.println();
+  }
+  while(client.connected() && !client.available()) delay(1); //waits for data
+  while (client.connected() || client.available()) { //connected or data available
+    char c = client.read(); //citaj bity z bufferu
+    Serial.print(c); //raw feed z webu vratane hlavicky
+    if (c==lf) x=(x+1); //pocitaj riadky
+    else if (x==12) readString += c; //zostav readstring
+   } 
+if(readString=="Zapni LED 1"){
  digitalWrite(led1, HIGH);
  }
- if(line=="Vypni LED 1"){
+ if(readString=="Vypni LED 1"){
  digitalWrite(led1, LOW);
  }
- if(line=="Zapni LED 2"){
+ if(readString=="Zapni LED 2"){
  digitalWrite(led2, HIGH);
  }
- if(line=="Vypni LED 2"){
+ if(readString=="Vypni LED 2"){
  digitalWrite(led2, LOW);
  }
- if(line=="Zapni LED 3"){
+ if(readString=="Zapni LED 3"){
  digitalWrite(led3, HIGH);
  }
- if(line=="Vypni LED 3"){
+ if(readString=="Vypni LED 3"){
  digitalWrite(led3, LOW);
  }
- if(line=="Zapni LED 4"){
+ if(readString=="Zapni LED 4"){
  digitalWrite(led4, HIGH);
  }
- if(line=="Vypni LED 4"){
+ if(readString=="Vypni LED 4"){
  digitalWrite(led4, LOW);
  }
- if(line=="Vypni"){
+ if(readString=="Vypni"){
 
  digitalWrite(led1, LOW);
  digitalWrite(led2, LOW);
  digitalWrite(led3, LOW);
  digitalWrite(led4, LOW);
  }
- if(line=="Zapni"){
+ if(readString=="Zapni"){
 
  digitalWrite(led1, HIGH);
  digitalWrite(led2, HIGH);
  digitalWrite(led3, HIGH);
  digitalWrite(led4, HIGH);
  }
+  readString = ("");
+  x=0;
+  client.stop(); //ukonc spojenie
+  delay(5000);
+} 
