744f61fe5b6664ab4998fa190399912fae13ecfc
Martin <martinius96@gmail.com>
2018-01-03 20:53:55 +0100

    Voice control W5100 Ethernet shield

--- arduino_en_version.ino
+++ arduino_en_version.ino
@@ -1,71 +1,51 @@
-// Author: Martin Chlebovec alias: martinius96
-// Donate: https://www.paypal.me/Chlebovec 
-// Personal website: https://arduino.php5.sk
-// Email contact: martinius96@gmail.com
-// Facebook contact: 100001242570317
-// Use under LICENSE 
-#include <ESP8266WiFi.h> //Arduino Core library
-#include <WiFiClientSecure.h> //Library Secure library for HTTPS connections (TLS) 
-const int led = 16; //pin D0 on NodeMCU board
-const char* ssid = "wifiname"; //wifi SSID name
-const char* password = "wifipassword"; //Wifi Password
-const char* host = "mywebsite.com"; //without https and www
-const int httpsPort = 443; //HTTPS port
-const char* fingerprint = "18 9f 6 1d b1 85 be e6 bd 73 c1 8d 04 63 58 99 f2 32 43 92"; // SHA1 fingerprint HTTPS of webserver
-void setup() {
- Serial.begin(115200); //serial read speed
- Serial.println();
- pinMode(led, OUTPUT);
- Serial.print("Connecting to wifi spot: ");
- Serial.println(ssid);
- WiFi.begin(ssid, password);
- while (WiFi.status() != WL_CONNECTED) {
- delay(500);
- Serial.print(".");
- }
- Serial.println("");
- Serial.println("WiFi connected");
- Serial.println("IP address: ");
- Serial.println(WiFi.localIP()); //Print IP address that NodeMCU get from DHCP server
+#include <SPI.h>
+#include <Ethernet.h>
+int led = 6;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //physical mac address
+char serverName[] = "www.mywebsite.com"; // zoomkat's test web page server
+IPAddress ip(192, 168, 2, 40);
+EthernetClient client;
+String readString;
+int x=0; //for counting line feeds
+char lf=10; //line feed character
+void setup(){
+pinMode(led, OUTPUT);
+ if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    Ethernet.begin(mac, ip);
+  }
+  Serial.begin(9600); 
 }
-void loop() {
- WiFiClientSecure client;
- Serial.print("Connecting to ");
- Serial.println(host);
- if (!client.connect(host, httpsPort)) {
- Serial.println("connection refused/unsucess");
- return;
- }
- if (client.verify(fingerprint, host)) {
- Serial.println("certificate matches");
- } else {
- Serial.println("certifficate didnt match");
- }
-String url = "/PHP/translations.txt"; //connect to that url of your webpage, link after host.
- Serial.print("Request to address: ");
- Serial.println(url);
- client.print(String("GET ") + url + " HTTP/1.1\r\n" +
- "Host: " + host + "\r\n" +
- "User-Agent: NodeMCU\r\n" +
- "Connection: close\r\n\r\n");
- Serial.println("Request taken");
- while (client.connected()) {
- String line = client.readStringUntil('\n');
-
- if (line == "\r") {
- Serial.println("Response accepted");
- break;
- }
- }
- String line = client.readStringUntil('\n');
- Serial.println("Response variable: ");
- Serial.println("==========");
- Serial.println(line);
- if(line=="Turn on light"){ //if variable is Turn on light turn on LED! (light)
- digitalWrite(led, HIGH);
- }else if(line=="Turn off light"){
- digitalWrite(led, LOW);
- }else{
+void loop(){
+if (client.connect(serverName, 80)) {  //starts client connection, checks for connection
+    Serial.println("connected");
+    client.println("GET /PHP_en/translations.txt HTTP/1.1"); //download text
+    client.println("Host: www.mywebsite.com");
+    client.println("Connection: close");  //close 1.1 persistent connection  
+    client.println(); //end of get request
+  } 
+  else {
+    Serial.println("connection failed"); //error message if no client connect
+    Serial.println();
+  }
+  while(client.connected() && !client.available()) delay(1); //waits for data
+  while (client.connected() || client.available()) { //connected or data available
+    char c = client.read(); //gets byte from ethernet buffer
+    Serial.print(c); //prints raw feed for testing
+    if (c==lf) x=(x+1); //counting line feeds
+    else if (x==12) readString += c; //building readString
+   } 
+if(readString=="Turn on light"){
+  digitalWrite(led, HIGH); 
+  }else if(readString=="Turn off light"){
+  digitalWrite(led, LOW);  
+    }
+    else{
  Serial.println("Unsupported string/variable. Cannot do action. Please repeat your request on site.");
  }
- }
+  readString = ("");
+  x=0;
+  client.stop(); //stop client
+  delay(5000);
+} 
+
