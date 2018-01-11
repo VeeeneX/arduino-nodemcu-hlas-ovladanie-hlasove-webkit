dfea5f03f6fb3d425e1079fc560df64f63c38bb3
Martin <martinius96@gmail.com>
2018-01-11 19:47:34 +0100

    Update ajax.php

--- PHP_en/ajax.php
+++ PHP_en/ajax.php
@@ -13,15 +13,10 @@
   }elseif(strlen($translation) > 50){
     echo "Your string is too long!";
   }else{
-   
-
-    
-                   if ($translation=="turn on light" || $translation=="Turn on light" || $translation=="Turn on led" || $translation=="Turn on let" || $translation=="Turn on LED" || $translation=="turn on LED" || $translation=="turn on let" || $translation=="turn on led"){
-          //translate multiple options of voice to one variable that we compare with NodeMCU
-      file_put_contents("translations.txt", 'Turn on light');
-     }else if($translation=="turn off light" || $translation=="Turn off light" || $translation=="Turn off led" || $translation=="Turn off let" || $translation=="Turn off LED" || $translation=="turn off LED" || $translation=="turn off let" || $translation=="turn off led"){
-          //translate multiple options of voice to one variable that we compare with NodeMCU
-               file_put_contents("translations.txt", 'Turn off light');
+      if ($translation=="turn on" || $translation=="Turn on"){
+      file_put_contents("translations.txt", 'Turn on');
+           }else if($translation=="turn off" || $translation=="Turn off"){
+               file_put_contents("translations.txt", 'Turn off');
      }
      }
      
