391a8d48b4ee6ceab861c7d22522c6b1d9ba61de
Martin <martinius96@gmail.com>
2018-01-11 19:37:07 +0100

    Update ajax.php

--- PHP_cz/ajax.php
+++ PHP_cz/ajax.php
@@ -17,11 +17,11 @@
 	 exit();
   }else{
     
-            if ($preklad=="Zapni" || $preklad=="Zapnout"){
+            if ($preklad=="Zapni" || $preklad=="Zapnout" || $preklad=="zapni" || $preklad=="zapnout"){
      
       file_put_contents("preklady.txt", 'Zapni');
      }
-     else if ($preklad=="Vypni" || $preklad=="Vypnout"){
+     else if ($preklad=="Vypni" || $preklad=="Vypnout" || $preklad=="vypni" || $preklad=="vypnout"){
               
       file_put_contents("preklady.txt", 'Vypni');
      }
