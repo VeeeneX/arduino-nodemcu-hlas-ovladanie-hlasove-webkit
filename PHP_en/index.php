be798001f8193f89a09057ac5f8f6be5e4342605
Martin <martinius96@gmail.com>
2018-01-11 19:46:57 +0100

    Update index.php

--- PHP_en/index.php
+++ PHP_en/index.php
@@ -32,11 +32,9 @@
    
 
     
-           if ($translation=="turn on light" || $translation=="Turn on light" || $translation=="Turn on led" || $translation=="Turn on let" || $translation=="Turn on LED" || $translation=="turn on LED" || $translation=="turn on let" || $translation=="turn on led"){
-          //translate multiple options of voice to one variable that we compare with NodeMCU
-      file_put_contents("translations.txt", 'Turn on light');
-     }else if($translation=="turn off light" || $translation=="Turn off light" || $translation=="Turn off led" || $translation=="Turn off let" || $translation=="Turn off LED" || $translation=="turn off LED" || $translation=="turn off let" || $translation=="turn off led"){
-          //translate multiple options of voice to one variable that we compare with NodeMCU
+           if ($translation=="turn on" || $translation=="Turn on"){
+      file_put_contents("translations.txt", 'Turn on');
+           }else if($translation=="turn off" || $translation=="Turn off"){
                file_put_contents("translations.txt", 'Turn off light');
      }
      }
