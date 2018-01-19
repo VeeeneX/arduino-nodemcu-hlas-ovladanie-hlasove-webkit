9a5c932798e0c495aa1353d620894c0026f50c08
VeeeneX <VeeeneX@users.noreply.github.com>
2018-01-19 20:55:51 +0100

    Make it nice :)

--- PHP_sk/ajax.php
+++ PHP_sk/ajax.php
@@ -5,24 +5,20 @@
 // Email kontakt: martinius96@gmail.com
 // Facebook kontakt: 100001242570317
 // Používajte v súhade s licenciou!
-   $preklad = $_POST['preklad'];
-   $preklad = htmlspecialchars($preklad);
-     $preklad = trim( $preklad );
-     if($preklad == ""){
-    echo "Prázdne pole ťažko zapíšem :-)!";
-    exit();
-	
-  }elseif(strlen($preklad) > 50){
-    echo "Příliš dlhé slovo/spojenie!";
-	 exit();
-  }else{   
-        if ($preklad=="Zapni" || $preklad=="Zapnúť" || $preklad=="zapni" || $preklad=="zapnúť"){
-     
+
+$preklad = $_POST['preklad'];
+$preklad = htmlspecialchars($preklad);
+
+$preklad = trim( $preklad );
+
+if ($preklad == "") {
+   echo "Prázdne pole ťažko zapíšem :-)!";
+} else if (strlen($preklad) > 50) {
+   echo "Příliš dlhé slovo/spojenie!";
+} else {   
+   if ($preklad == "Zapni" || $preklad == "Zapnúť" || $preklad == "zapni" || $preklad == "zapnúť") {
       file_put_contents("preklady.txt", 'Zapni');
-     }
-     else if ($preklad=="Vypni" || $preklad=="Vypnúť" || $preklad=="vypni" || $preklad=="vypnúť"){
-              
+   } else if ($preklad == "Vypni" || $preklad == "Vypnúť" || $preklad == "vypni" || $preklad == "vypnúť") {
       file_put_contents("preklady.txt", 'Vypni');
-     }
-     }
-?>
+   }
+}
